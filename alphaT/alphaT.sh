ead2d31d00bf441f3b34d4fdea23ea3bf89b4c6f
Evgenii Sharaborin <weugene@mail.ru>
2019-08-08 14:42:27 +0300

    MacOS->github

---
+++ alphaT/alphaT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=10
+
+#PBS -l pmem=10000MB
+
+#PBS -l walltime=46:00:00
+
+#PBS -q batch
+
+cd $HOME/basilisk/work/no_wetting_satur_straight
+mpirun -np 10 ./a.out
+
